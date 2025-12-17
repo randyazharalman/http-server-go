84c40442e12fd523c9e0156574fc10bcfd86fb40
randyazharalman <azharalman17@gmail.com>
2025-12-17 15:03:01 +0700

    CH2L2: Stateful Handlers

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+
 func main() {
-	mux := http.NewServeMux()
+
+		apiCfg := &apiConfig{}
+	
+		mux := http.NewServeMux()
 
     mux.HandleFunc("/healthz", healthzHandler)
+		mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+		mux.HandleFunc("/reset", apiCfg.resetHandler)
 
     fileServer := http.FileServer(http.Dir("."))
-    mux.Handle("/app/",http.StripPrefix("/app", fileServer))
+    mux.Handle("/app/",apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -23,6 +36,26 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error starting server: %v\n", err)
 	}
+}
+
+func(cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func(cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits:= cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+func(cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request){
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)	
+	w.Write([]byte("Hits: reset to 0"))
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request){
