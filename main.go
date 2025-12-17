6f944ad25b1514c06ccfee76fd661911420ce9a1
randyazharalman <azharalman17@gmail.com>
2025-12-17 15:35:30 +0700

    ch2l3: routing

--- main.go
+++ main.go
@@ -7,24 +7,22 @@ import (
 	"sync/atomic"
 )
 
-
 type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-
 func main() {
 
-		apiCfg := &apiConfig{}
-	
-		mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-    mux.HandleFunc("/healthz", healthzHandler)
-		mux.HandleFunc("/metrics", apiCfg.metricsHandler)
-		mux.HandleFunc("/reset", apiCfg.resetHandler)
+	mux := http.NewServeMux()
 
-    fileServer := http.FileServer(http.Dir("."))
-    mux.Handle("/app/",apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHandler)
+
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -38,28 +36,44 @@ func main() {
 	}
 }
 
-func(cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func(cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request){
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	hits:= cfg.fileserverHits.Load()
+	hits := cfg.fileserverHits.Load()
 	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
 }
-func(cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request){
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)	
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits: reset to 0"))
 }
 
-func healthzHandler(w http.ResponseWriter, r *http.Request){
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte("OK"))
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
